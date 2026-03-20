ef0daddb52e77381866eacbb55f0b024f7240a30
logancford64-dot <logancford64@gmail.com>
2026-03-19 22:48:13 -1000

    pathplanner paths added

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -24,60 +24,37 @@ public class LauncherSubsystem extends SubsystemBase {
   private double m_calculatedRPM = 0.0;
   public static final int[] validTags = {9, 10, 25, 26};
 
-  //  PID VALUES (tunable from Shuffleboard)
-  private double kP = 0.0003; //was .00002
-  private double kI = 0.0;
-  private double kD = 0.0028; // was .0021
-  private double kFF = 0.00195;
-
-  // Track previous values (prevents CAN spam)
-  private double prevP = kP;
-  private double prevI = kI;
-  private double prevD = kD;
-  private double prevFF = kFF;
-
   public LauncherSubsystem() {
-
     SparkFlexConfig motorConfigL = new SparkFlexConfig();
     SparkFlexConfig motorConfigR = new SparkFlexConfig();
 
-    // Initial config (will be overridden by Shuffleboard)
+   // p org was 0.005
+   // ff was org 0.000147
     motorConfigL.closedLoop
-        .p(kP)
-        .d(kD)
-        .velocityFF(kFF);
-
+        .p(0.0001) 
+        .d(0.0001)
+        .velocityFF(0.0002);
+    
     motorConfigL
         .inverted(false)
-        .idleMode(IdleMode.kCoast)
+        .idleMode(IdleMode.kCoast) 
         .smartCurrentLimit(60);
 
-    // Motor R follows L (inverted)
+    // Motor R Configuration: Follows Motor L but inverted
     motorConfigR
-        .follow(launcherMotorL, true)
+        .follow(launcherMotorL, true) // 'true' inverts the follower
         .idleMode(IdleMode.kCoast)
         .smartCurrentLimit(60);
 
+    // Apply configurations
     launcherMotorL.configure(motorConfigL, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     launcherMotorR.configure(motorConfigR, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    // Initialize Shuffleboard values
-    //SmartDashboard.putNumber("Shooter/Target RPM", m_calculatedRPM);
-     SmartDashboard.putNumber("Shooter/Actual L RPM", launcherMotorL.getEncoder().getVelocity());
-     SmartDashboard.putNumber("Shooter/Actual R RPM", launcherMotorR.getEncoder().getVelocity());
-    SmartDashboard.putNumber("Shooter/P", kP);
-    SmartDashboard.putNumber("Shooter/D", kD);
-    SmartDashboard.putNumber("Shooter/FF", kFF);
-  
-    
 
     stopLauncher();
   }
 
   @Override
   public void periodic() {
-
-    // ================= LIMELIGHT RPM CALC =================
     boolean hasTarget = LimelightHelpers.getTV("limelight-launch");
     int currentTargetID = (int) LimelightHelpers.getFiducialID("limelight-launch");
     boolean isValidTag = Arrays.stream(validTags).anyMatch(id -> id == currentTargetID);
@@ -86,50 +63,38 @@ public class LauncherSubsystem extends SubsystemBase {
         double[] botPose = LimelightHelpers.getBotPose_TargetSpace("limelight-launch");
         double tz = Math.abs(botPose[2]);
 
-        double tzMin = 0.1; 
+      
+        double tzMin = .8; 
         double tzMax = 3.0;
-        double rpmMin = 0.0; 
+        double rpmMin = 4000; 
         double rpmMax = 6784.0; 
 
         double clampedTz = MathUtil.clamp(tz, tzMin, tzMax);
         m_calculatedRPM = rpmMin + (clampedTz - tzMin) * (rpmMax - rpmMin) / (tzMax - tzMin);
-    }
+    } 
 
-    // ================= READ PID FROM SHUFFLEBOARD =================
-    kP = SmartDashboard.getNumber("Shooter/P", kP);
-    kI = SmartDashboard.getNumber("Shooter/I", kI);
-    kD = SmartDashboard.getNumber("Shooter/D", kD);
-    kFF = SmartDashboard.getNumber("Shooter/FF", kFF);
-
-    
-double shootingrpm = 4750;
-    // ================= DASHBOARD OUTPUT =================
-    SmartDashboard.putNumber("Shooter/Target RPM", shootingrpm);
+    // Dashboard updates
+    SmartDashboard.putNumber("Shooter/Target RPM", m_calculatedRPM);
     SmartDashboard.putNumber("Shooter/Actual L RPM", launcherMotorL.getEncoder().getVelocity());
     SmartDashboard.putNumber("Shooter/Actual R RPM", launcherMotorR.getEncoder().getVelocity());
   }
 
-  // ================= CONTROL METHODS =================
 
   public void runVelocityFromLimelight() {
     closedLoopControllerL.setReference(m_calculatedRPM, ControlType.kVelocity);
   }
 
-  public void runFixedShooting( ) {
-    closedLoopControllerL.setReference(4750, ControlType.kVelocity);
+
+  public void runFixedShooting() {
+    closedLoopControllerL.setReference(4800.0, ControlType.kVelocity);
   }
 
-  public void runFromDashboard() {
-    double setpoint = SmartDashboard.getNumber("Shooter/Setpoint", 0);
-    closedLoopControllerL.setReference(setpoint, ControlType.kVelocity);
-  }
 
   public void runLauncher(double rpm) {
     closedLoopControllerL.setReference(rpm, ControlType.kVelocity);
   }
 
   public void stopLauncher() {
-    launcherMotorL.stopMotor();
+    closedLoopControllerL.setReference(0, ControlType.kVelocity);
   }
-
 }
